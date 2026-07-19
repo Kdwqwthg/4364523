d0da74a0ba081d74fc676f34fce55b476ef24078
Kdwqwthg <poporaum20@gmail.com>
2026-07-19 12:55:55 +1000

    Add files via upload

---
+++ Online Script/KeySystem.lua
@@ -0,0 +1,13 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "⚠️Draconic Hub⚠️",
+    Text = "The script is closed for updating...",
+    Icon = "rbxassetid://102225156206159",
+    Duration = 15
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "⚠️Draconic Hub⚠️",
+    Text = "Скрипт закрыт на обновление...",
+    Icon = "rbxassetid://102225156206159",
+    Duration = 15
+})
