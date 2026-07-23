b71d6bc913acb980e5d35cdd90a57b96e2b6e6f4
Kdwqwthg <poporaum20@gmail.com>
2026-07-23 18:59:00 +1000

    Update ScriptMobile.lua

--- Online Script/ScriptMobile.lua
+++ Online Script/ScriptMobile.lua
@@ -1,5 +1,5 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Draconic Hub 9",
+    Title = "Draconic Hub 1",
     Text = "Welcome Draconic Hub Remake",
     Icon = "rbxassetid://102225156206159",
     Duration = 7
