15cb76c4e4195403eafb564f43ad428f8aceea32
Kdwqwthg <poporaum20@gmail.com>
2026-08-07 04:10:25 +1000

    Update ScriptMobile.lua

--- Online Script/ScriptMobile.lua
+++ Online Script/ScriptMobile.lua
@@ -26,7 +26,7 @@ local Window = Fluent:CreateWindow({
 })
 
 local FLOATING_BTN_URL = "https://raw.githubusercontent.com/Kdwqwthg/4364523/refs/heads/main/Online%20Script/FlyBytton.lua"
-local GRADIENT_BUTTON_URL = "https://raw.githubusercontent.com/Kdwqwthg/4364523/refs/heads/main/Online%20Script/CreateGradientButton.lua"
+local GRADIENT_BUTTON_URL = "https://raw.githubusercontent.com/Kdwqwthg/4364523/refs/heads/main/Online%20Script/CreateGradientButton.lu"
 local SIMPLE_TIMER_URL = "https://raw.githubusercontent.com/Kdwqwthg/4364523/refs/heads/main/Online%20Script/CreateSimpleTimer.lu"
 local floatingBtnSource
 pcall(function()
